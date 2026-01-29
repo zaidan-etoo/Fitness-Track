4ae7622422b9f1be3b1edd543fbe8318946fad23
zaidan-etoo <daffazaidanetoo@gmail.com>
2026-01-29 08:07:32 +0700

    Add files via upload

---
+++ database/migrations/2026_01_28_081652_create_workouts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+    Schema::create('workouts', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->string('name');
+        $table->string('type');
+        $table->integer('duration'); // menit
+        $table->integer('calories');
+        $table->date('date');
+        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workouts');
+    }
+};
